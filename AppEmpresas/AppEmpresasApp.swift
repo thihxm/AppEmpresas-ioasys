b1c393141c5d452a69dd654a8c9b76a8519669c0
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-12-11 16:56:13 -0300

    Initial Commit

---
+++ AppEmpresas/AppEmpresasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEmpresasApp.swift
+//  AppEmpresas
+//
+//  Created by Thiago Medeiros on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AppEmpresasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
